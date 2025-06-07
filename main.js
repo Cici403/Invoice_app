a55ab1788078578c9c79f7a73da875e80fe90f45
Cici403 <christian.schreter@gmail.com>
2025-06-07 12:20:05 +0200

    start brand

---
+++ main.js
@@ -0,0 +1,17 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+let mainWindow;
+
+app.whenReady().then(() => {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  mainWindow.loadFile("src/index.html");
+});
